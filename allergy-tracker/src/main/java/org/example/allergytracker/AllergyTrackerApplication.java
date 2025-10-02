5da9b4ba43715bf70e351e8584777c8150f9f21f
Bartolomeooo <bartoszkawiak@gmail.com>
2025-10-02 23:48:43 +0200

    Initial commit

---
+++ allergy-tracker/src/main/java/org/example/allergytracker/AllergyTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.example.allergytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllergyTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllergyTrackerApplication.class, args);
+    }
+
+}
