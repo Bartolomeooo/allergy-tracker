08e27382096b6c6c8fef19044c8b8cd736d4ff0e
Maksymilian74 <273007@student.pwr.edu.pl>
2025-10-10 17:27:31 +0200

    Configure Eslint and Prettier

--- allergy-tracker-ui/eslint.config.js
+++ allergy-tracker-ui/eslint.config.js
@@ -1,23 +1,41 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import {defineConfig, globalIgnores} from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import vitest from '@vitest/eslint-plugin';
+import importX from 'eslint-plugin-import-x';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2024,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    plugins: {
+      'import-x': importX,
+      'unused-imports': unusedImports,
     },
   },
-])
+
+  {
+    files: ['**/*.{test,spec}.{ts,tsx}'],
+    ...vitest.configs.recommended,
+  },
+]);
